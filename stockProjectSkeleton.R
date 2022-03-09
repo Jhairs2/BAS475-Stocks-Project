342f7db4717fde6b5fc159ddb5da2809fd25b0f2
Jhairs2 <jhairst2@vols.utk.edu>
2022-03-09 18:16:39 -0500

    Preparing code for merge

--- stockProjectSkeleton.R
+++ stockProjectSkeleton.R
@@ -1,18 +1,37 @@
+
 library(shiny)
 library(shinydashboard)
-library(fpp3)
+library(shinycssloaders)
 library(quantmod)
 library(ggplot2)
 library(plotly)
+library(tidyverse)
+library(tidyquant)
+library(shinyWidgets)
+library(dashboardthemes)
+
+
+# Configuring settings as per tidyquant tutorial
+options("getSymbols.warning4.0"=FALSE)
+options("getSymbols.yahoo.warning"=FALSE)
+
+
+
+# Define function to split names on dot and only keep after dot text
+clean_names <- function(stocks) {
+  split_names = strsplit(names(stocks), split = '.', fixed = TRUE)
+  vapply(split_names, function(x) x[2], character(1))
+}
+
 
 # Getting stock symbols and creating date range
 stockNames <- stockSymbols()[,1]
 start <- as.Date("2010-01-01")
 end <- Sys.Date()
 
-ui <- dashboardPage(
-    
-    dashboardHeader(),
+ui <- dashboardPage( skin = "yellow", 
+                     
+                     dashboardHeader( title = "Stock Analysis"),
     
     dashboardSidebar(
         
@@ -28,12 +47,16 @@ ui <- dashboardPage(
     ),
     
     dashboardBody(
-     # While plot is being created a loading screen spinner will appear
-    withSpinner(
-     ( plotlyOutput("plot") 
+        shinyDashboardThemes( theme = "grey_dark" ),
+        # While plot is being created a loading screen spinner will appear
+        withSpinner(
+            plotlyOutput("plot")
+            ),
+        withSpinner(
+            plotlyOutput("candleStick")
+            )
+                       
     ) 
-    )
-)
 )
 
 
@@ -48,28 +71,42 @@ server <- function(input, output, session) {
     chosenStock <- reactive( {
         stockInfo <<- getSymbols(input$chooseStock, src = "yahoo", from = input$chooseDate[1], end = input$chooseDate[2], 
                                  auto.assign = FALSE)
+        names(stockInfo) <<- clean_names(stockInfo)
+        stockInfo
         
     })
     
     # Creating interactive plot for stock at specified range
-    output$plot <- renderPlotly ({
-
-      # Graph will not appear until stock has been selected and loaded in server
-    req(input$chooseStock)
+  output$plot <- renderPlotly ({
+    req(chosenStock(), stockInfo)
+      P1 <-  chosenStock() %>%
+        ggplot(aes(x = index(stockInfo), y = stockInfo[,4], 
+                   text = paste("Date: ", date(stockInfo),
+                                "<br>Stock Price: $", stockInfo[,4]),
+                   group = "Date")) +
+        geom_line(color = "orange") + 
+        labs( title =  paste("Closing Price for", input$chooseStock),
+              y = "Stock Price",
+              x = "Date")
+      
+      ggplotly(P1, tooltip = "text")
+  })
+  
+  output$candleStick <- renderPlotly({
+    req(chosenStock(), stockInfo)
     
-    P1 <-  chosenStock() %>%
-      ggplot(aes(x = index(stockInfo), y = stockInfo[,4], 
-                 text = paste("Date: ", date(stockInfo),
-                              "<br>Stock Price: $", stockInfo[,4]),
-                 group = "Date")) +
-      geom_line(color = "orange") + 
-      labs( title =  paste("Closing Price for", input$chooseStock),
-            y = "Stock Price",
-            x = "Date")
+    dat <- as.data.frame(stockInfo)
+    dat$date <- index(stockInfo)
+    dat <- subset(dat, date >= "2016-01-01")
     
-    ggplotly(P1, tooltip = "text")
-    })
-    
+    fig <- plot_ly(dat, x = ~date, xend = ~date, color = ~Close > Open,
+                   colors = c("red", "forestgreen"), hoverinfo = "none") 
+    fig <- fig %>% add_segments(y = ~Low, yend = ~High, size = I(1)) 
+    fig <- fig %>% add_segments(y = ~Open, yend = ~Close, size = I(3)) 
+    fig <- fig %>% layout(showlegend = FALSE, yaxis = list(title = "Price")) 
+    fig <- fig %>% rangeslider()
+    fig
+  })
     
 }
 
