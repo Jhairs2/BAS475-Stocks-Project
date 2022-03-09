a903c76a89d1d913a963b69c7cd9c09e4b4c8dc4
Jhairs2 <jhairst2@vols.utk.edu>
2022-03-09 15:44:09 -0500

    Added loading feature, fixed date bug, and fixed up labels

--- stockProjectSkeleton.R
+++ stockProjectSkeleton.R
@@ -8,7 +8,7 @@ library(plotly)
 # Getting stock symbols and creating date range
 stockNames <- stockSymbols()[,1]
 start <- as.Date("2010-01-01")
-end <- as.Date("2022-03-01")
+end <- Sys.Date()
 
 ui <- dashboardPage(
     
@@ -17,19 +17,21 @@ ui <- dashboardPage(
     dashboardSidebar(
         
         # User Will choose stock Here
-        selectizeInput("chooseStock", choices = NULL, label = "Choose a stock", selected = stockNames[1]),
+        selectizeInput("chooseStock", choices = NULL, label = h3("Choose a stock")),
         
         # User can choose date range for stock
         dateRangeInput(
             "chooseDate",
-            label = "Choose a Date"
+            label = h3("Choose a Date")
         )
         
     ),
     
     dashboardBody(
-        
-        plotlyOutput("plot")
+     # While plot is being created a loading screen spinner will appear
+    withSpinner(
+     ( plotlyOutput("plot") 
+    ) 
     )
 )
 
@@ -38,26 +40,33 @@ server <- function(input, output, session) {
     
     # Updates stock choices and date selections in server
     updateSelectizeInput(session, "chooseStock", choices = stockNames, selected = stockNames[1], server = T)
-    updateDateRangeInput(session, "chooseDate",  start = start, end = Sys.Date(), max = Sys.Date())
+    updateDateRangeInput(session, "chooseDate",  start = start, end = Sys.Date(), min = start, max = Sys.Date())
     
     
     # Creating variable for stock information. The '<<-' is to make the variable global
     chosenStock <- reactive( {
-        stockInfo <<- getSymbols(input$chooseStock, src = "yahoo", from = input$chooseDate, end = input$chooseDate, 
+        stockInfo <<- getSymbols(input$chooseStock, src = "yahoo", from = input$chooseDate[1], end = input$chooseDate[2], 
                                  auto.assign = FALSE)
         
     })
     
     # Creating interactive plot for stock at specified range
     output$plot <- renderPlotly ({
-      P1 <-  chosenStock() %>%
-        ggplot(aes(x = date(stockInfo), y = stockInfo[,4])) +
-            geom_line() + 
-            labs( title =  paste("Closing Price for", input$chooseStock),
-                                y = "Stock Price",
-                                x = "Date")
-      ggplotly(P1)
-      
+
+      # Graph will not appear until stock has been selected and loaded in server
+    req(input$chooseStock)
+    
+    P1 <-  chosenStock() %>%
+      ggplot(aes(x = index(stockInfo), y = stockInfo[,4], 
+                 text = paste("Date: ", date(stockInfo),
+                              "<br>Stock Price: $", stockInfo[,4]),
+                 group = "Date")) +
+      geom_line(color = "orange") + 
+      labs( title =  paste("Closing Price for", input$chooseStock),
+            y = "Stock Price",
+            x = "Date")
+    
+    ggplotly(P1, tooltip = "text")
     })
     
     
